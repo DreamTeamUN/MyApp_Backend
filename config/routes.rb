04780fd71aee6c1ad64b01866df9fc966ee7de0f
Angel Corredor <adcorredorm@unal.edu.co>
2018-10-07 11:52:47 -0500

    Esquema inicial de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,79 @@
 Rails.application.routes.draw do
-  resources :entradas
-  resources :nivel_accesos
-  resources :progresos
-  resources :docentes
-  resources :tutors
-  resources :estudiantes
-  resources :estudiante_aulas
-  resources :aulas
+
+  #get 'tipo/:id', to: 'tipo_usuarios#show' #Para crear rutas personalizadas
+
+  resources :usuarios do
+    resources :entradas
+  end
+
+  resources :usuarios do
+      resources :registro_actividads
+  end
+
+  resources :nivel_accesos do
+    resources :entradas
+  end
+
+  resources :estudiantes do
+      resources :progresos
+  end
+
+  resources :estudiantes do
+      resources :puntuacions
+  end
+
+  resources :leccions do
+      resources :progresos
+  end
+
+  resources :tutors do
+      resources :estudiantes
+  end
+
+  resources :docentes do
+      resources :aulas
+  end
+
+  resources :programas do
+      resources :aulas
+  end
+
+  resources :programas do
+    resources :leccions
+  end
+
+  resources :leccions do
+    resources :frases
+  end
+
+  resources :leccions do
+    resources :puntuacions
+  end
+
+  resources :tipo_juegos do
+    resources :puntuacions
+  end
+
+  resources :tipo_juego do
+    resources :archivo_juegos
+  end
+
+  resources :tipo_actividads do
+      resources :registro_actividads
+  end
+
+  resources :tipo_usuarios do
+    resources :frase_pnls
+  end
+
+  resources :tipo_usuarios do
+    resources :usuarios
+  end
+
   resources :docente_programas
-  resources :programas
-  resources :leccions
-  resources :frases
-  resources :puntuacions
-  resources :tipo_juegos
-  resources :archivo_juegos
-  resources :archivos
-  resources :tipo_actividads
-  resources :registro_actividads
+  resources :estudiante_aulas
+  resources :archivos #Solo get y create
   resources :frase_pnls
-  resources :tipo_usuarios
-  resources :usuarios
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
