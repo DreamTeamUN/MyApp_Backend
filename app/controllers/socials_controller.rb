d060a955901150a69717b58b3de94feea7912b49
Juan Becerra <jsbecerrab@unal.edu.co>
2018-11-28 13:56:39 -0500

    problema de arquitectura en redes sociales resuelto

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -1,23 +1,34 @@
+require 'httparty'
+require 'json'
+
 class SocialsController < ApplicationController
+  include HTTParty
   def create
+    url = "https://www.googleapis.com/oauth2/v3/tokeninfo?"
+    response= HTTParty.get url, :headers=>{"Authorization"=>"Bearer \"#{params["token"]}\""}
+    response = response.parsed_response
+    if response["email_verified"] == "true"
   # puts params
-    @user = Usuario.find_by(email: params["email"] )
+      @user = Usuario.find_by(email: params["email"] )
 
-   if @user == nil
-       p = Faker::Internet.password(8)
-       @user = Usuario.create(user: params["name"],email: params["email"],
-        password: p, password_confirmation: p, nombre: params["name"], tipo_usuario_id: 1 )
-       if @user.save
-           knock_token = Knock::AuthToken.new payload: { sub: @user.id }
-           render json:  knock_token
-       else
+      if @user == nil
+        p = Faker::Internet.password(8)
+        @user = Usuario.create(user: params["name"],email: params["email"],
+          password: p, password_confirmation: p, nombre: params["name"], tipo_usuario_id: 1 )
+          if @user.save
+            knock_token = Knock::AuthToken.new payload: { sub: @user.id }
+            render json:  knock_token
+          else
 
-       end
-   else
-    knock_token = Knock::AuthToken.new payload: { sub: @user.id }
-   render json:  knock_token
-   end
+          end
+      else
+        knock_token = Knock::AuthToken.new payload: { sub: @user.id }
+        render json:  knock_token
+      end
+    else
+      render json: {}, status: 401
 
+    end
 
  end
 end
